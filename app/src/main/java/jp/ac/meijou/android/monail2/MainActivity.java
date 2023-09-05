9d275e2c9d1fa353a089b1ebc18277fe3e5308e8
Nisuimura Wakana <221205123@ccmailg.meijo-u.ac.jp>
2023-09-05 13:38:54 +0900

    layout部分を大幅に変更した

--- app/src/main/java/jp/ac/meijou/android/monail2/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/monail2/MainActivity.java
@@ -3,12 +3,54 @@ package jp.ac.meijou.android.monail2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
+
+import jp.ac.meijou.android.monail2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(view -> {
+            binding.text.setText(R.string.my_name);
+        });
+
+        binding.button2.setOnClickListener(view ->{
+            binding.imageView.setImageResource(R.drawable.ic_androidred);
+        });
+
+        binding.button3.setOnClickListener(view ->{
+            var text = binding.editText.getText().toString();
+            binding.text.setText(text);
+        });
+
+        binding.editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.text.setText(s.toString());
+            }
+        });
+
+        binding.text.setText(R.string.app_name);
+
     }
+
+
 }
